4189e997aa0728c9e67724b8c23a4290111391a9
George Nita <georgenita.dev@gmail.com>
2022-06-06 16:34:19 +0200

    associations and validations done

--- app/models/choice_point.rb
+++ app/models/choice_point.rb
@@ -1,3 +1,8 @@
 class ChoicePoint < ApplicationRecord
   belongs_to :user
+  has_many :options
+  has_many :tags
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :deadline, presence: true
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,5 @@
 class Option < ApplicationRecord
   belongs_to :choice_point
+  has_many :votes
+  validates :description, presence: true
 end
