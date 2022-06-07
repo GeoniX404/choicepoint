31998ae7e0799c28101afa802f1b43e81b77dd3a
George Nita <georgenita.dev@gmail.com>
2022-06-07 15:41:43 +0200

    corrected error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :choice_points, only: %i[index show new create] do
-    resources :options, only: [create]
+    resources :options, only: %i[create]
   end
 end
