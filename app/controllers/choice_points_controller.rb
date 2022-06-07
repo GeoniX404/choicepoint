11efcb3bdc64cd98e937381cfffcf23cec40f9ef
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-06-07 14:03:56 +0200

    Coded choice_points#show

--- app/controllers/choice_points_controller.rb
+++ app/controllers/choice_points_controller.rb
@@ -1,3 +1,7 @@
 class ChoicePointsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+
+  def show
+    @choice_point = ChoicePoint.find(params[:id])
+  end
 end
