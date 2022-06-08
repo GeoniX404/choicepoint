2ce7d510f0715844a09e61128df95645ba66d3c8
Chris Wall <cbwall24@gmail.com>
2022-06-08 13:44:16 +0300

    added vote method to options controller

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,14 @@
 class OptionsController < ApplicationController
+  def create
+    @option = Option.find(params[:id])
+    @vote = @option.votes.create
+    @vote.user = current_user
+    redirect_to choice_points_path(@choice_points)
+  end
+
+  # private
+
+  # def vote_params
+  #   params.require(:vote).permit(:user_id, :option_id)
+  # end
 end
