d0455485f9d448cb39cda56bcdbf3a6d93a7acf5
Chris Wall <cbwall24@gmail.com>
2022-06-09 18:57:25 +0300

    added associations to connect votes to choice points and started trending logic in choicepoint index view

--- app/models/choice_point.rb
+++ app/models/choice_point.rb
@@ -3,6 +3,7 @@ class ChoicePoint < ApplicationRecord
   belongs_to :user
   has_many :options, dependent: :destroy
   has_many :tags, dependent: :destroy
+  has_many :votes, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
   validates :deadline, presence: true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :option
   belongs_to :user
+  belongs_to :choice_point
+
+  scope :popular, -> { select('id, count(id) as count').group(:id).order('count desc').limit(10) }
 end
