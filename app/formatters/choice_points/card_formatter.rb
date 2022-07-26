aedd27eb79c5471e0bc59f132aab0b2a6e790556
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-07-26 12:52:52 +0200

    Add a choice point formatter
    This commit adds a CardFormatter class for choice points, with methods
    returning appropriate values for the regions of a card. For example,
    `ChoicePoint::CardFormatter#pre_heading` returns a string that will be
    displayed in the card's "pre-heading" position, which includes the name
    of the choice point's author. While decorator methods are content-
    oriented (e.g., a ChoicePointDecorator would be expected to have
    different methods from, say, an OptionDecorator), formatter methods are
    layout-oriented. Any type of resource that we might want to format as
    a card could have its own CardFormatter class, which would be expected
    to implement methods like `#pre-heading`, `#heading`, `#content`, etc.
    
    An instance of `ChoicePoint::CardFormatter` is created by calling
    `ChoicePointDecorator#card`. Within formatters (as with decorators),
    view helpers like `link_to` can be accessed as `h.link_to`.

---
+++ app/formatters/choice_points/card_formatter.rb
@@ -0,0 +1,34 @@
+module ChoicePoints
+  class CardFormatter < ApplicationFormatter
+    def initialize(choice_point, helper_proxy)
+      @choice_point = choice_point
+      super(helper_proxy)
+    end
+
+    def link
+      h.choice_point_path(@choice_point)
+    end
+
+    def pre_heading
+      "posted by #{@choice_point.user.name}"
+    end
+
+    def heading
+      @choice_point.link
+    end
+
+    def content
+      h.truncate(@choice_point.description, length: 116)
+    end
+
+    def image
+      h.calendarize(@choice_point.deadline do |deadline|
+        deadline.strftime("%b %d")
+      end)
+    end
+
+    def buttons
+      [@choice_point.vote_button, @choice_point.save_button]
+    end
+  end
+end
