3e5a77e8e87b3dac892941d623e6b00caeb998ff
Ross Godfrey <ross.m.godfrey@gmail.com>
2022-07-26 13:51:57 +0200

    Add `calendarize` helper
    This commit adds a `calendarize` helper, which wraps its argument in a
    calendar icon.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Wraps `content` in a calendar icon.
+  def calendarize(content)
+    tag.span(icon('far', 'calendar', class: 'fa-6x') + content,
+             class: ['fa-calendar-wrapper'])
+  end
 end
